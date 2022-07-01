65d79b51d6b8f56e622087691cf2efd48ee03fa9
tazakka <mymac@Jacks-MacBook-Pro.local>
2022-07-01 09:54:07 +0700

    selesai

--- challange/challange2.js
+++ challange/challange2.js
@@ -1,16 +1,12 @@
-function deretKaskus(i){ // i = parameter
-    let deret = [];
-    for ( i = 1; i < 10; i++){deret[i] = i*3}
-
-    if((i%5 == 0) && (i%6 == 0)){console.log('KASKUS')} //kondisi
-    else if (i%6 == 0){console.log('KUS')}
-    else if (i%5 == 0){console.log('KAS')} 
-    else {i};
-}
-// argumen belum dipanggil
-// perlu pakai return?
-//  value argumen != paramet
-//..rest argumen?
-console.log(deretKaskus(10)); // 10 = argumen
-
-//coba deretkaskus ==> mathround(mathfloor()*arguments)
+function deretKaskus(n){
+    let result =[];
+    for (j=1; j <= n; j++ ) {
+        i = j*3
+        if((i%5== 0) && (i%6==0)){result.push('KASKUS')}
+        else if (i%6 == 0){result.push('KUS')}
+        else if (i%5 == 0){result.push('KAS')} 
+        else {result.push(i)}
+    }return result
+} ;
+    
+console.log(deretKaskus(10));
