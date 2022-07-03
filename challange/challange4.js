5768fdd1f1b58f532eee0ab43816693d4c42709f
tazakka <mymac@Jacks-MacBook-Pro.local>
2022-07-03 21:07:05 +0700

    challange4

---
+++ challange/challange4.js
@@ -0,0 +1,24 @@
+function indexPrime(param1){
+    let prima = [2];
+    let i = 3;
+    let counter = 0;
+    do{
+        for(let j=2; j<i; j++){
+            if(i%j===0){
+                counter++;
+            }
+        }
+        if(counter===0){
+            prima.push(i);
+            i++;
+        }else{
+            i++;
+            counter=0;
+        }
+    }
+    while(prima.length<param1);
+    return prima[prima.length-1];
+}
+console.log(indexPrime(4))
+console.log(indexPrime(500))
+console.log(indexPrime(37786))
--- coretan/_challange2.js
+++ coretan/_challange2.js
@@ -1,12 +1,26 @@
-function deretKaskus(n){ // i = parameter
-    let deret = [];
-    for ( i = 1; i <= n; i++){deret[i] = i*3}
-    return 
-    
-/*
-    if((i%5 == 0) && (i%6 == 0)){console.log('KASKUS')} //kondisi
-    else if (i%6 == 0){console.log('KUS')}
-    else if (i%5 == 0){console.log('KAS')} 
-    else {i}; */
-}
-console.log(deretKaskus(10));
+function isPrime(number) {
+    if (typeof number !== 'number' || !Number.isInteger(number)) {
+      return false;
+    }
+  
+    if (number < 2) {
+      return false;
+    }
+  
+    if (number === 2) {
+      return true;
+    } else if (number % 2 === 0) {
+      return false;
+    }
+   
+    for (var i = 3; i*i <= number; i += 2) {
+      if (number % i === 0) {
+        return false;
+      }
+    }
+    return true;
+  
+  }
+  
+  console.log(isPrime(3));
+  console.log(isPrime(15485863));
