728945df8b50279fe173294f856c6e97153a7211
tazakka <mymac@Jacks-MacBook-Pro.local>
2022-07-05 10:02:15 +0700

    semangat

--- coretan/coret2.js
+++ coretan/coret2.js
@@ -1,31 +1,15 @@
 function stringManipulation(word) {
-    const vokal = ["a", "i", "u", "e", "o"];
-    let strings = word.split(" ");
-    let newStrings = strings.map((string) => {
-      if (vokal.includes(word.charAt(0))) {
-        return word;
-      } else {
-        let chars = string.split("");
-        let newChars = string.split("");
-  
-        for (const char of chars) {
-          if (!vokal.includes(char)) {
-            let firstChar = newChars.shift();
-            newChars.push(firstChar);
-          } else {
-            break;
-          }
-        }
-        let newString = newChars.join("") + "nyo";
-        return newString;
-      }
-    });
-  
-    console.log(newStrings.join(" "))
-  }
-  
-  stringManipulation('ayam')
-  stringManipulation('bebek')
-  
-  
-  
+  const vokal = ["a", "i", "u", "e", "o"];
+    if (vokal.includes(word.charAt(0))) {
+      console.log(word)
+    } else {
+      let letter = word.split();
+      firstLetter = letter.shift(0);
+      letter.push(firstLetter);
+      word = letter.join('')+"nyo"
+      console.log(word)
+}
+}
+stringManipulation('ayam bakar')
+stringManipulation('bebek')
+
--- coretan/latihanobjek.js
+++ coretan/latihanobjek.js
@@ -1,16 +1,29 @@
-function Mahasiswa(nama, energi){
-    let mahasiswa = {};
-    mahasiswa.nama = nama;
-    mahasiswa.energi = energi;
-    mahasiswa.makan = function (porsi){
+// Objek Literal
+
+const methodMahasiswa = {
+    makan : function (porsi){
         this.energi += porsi;
         console.log(`Halo ${this.nama}, selamat makan`)
-    }
-    mahasiswa.main = function (jam){
+    },
+
+    main : function (jam){
         this.energi -= jam;
         console.log(`Halo ${this.nama}, selamat bermain`)
+    },
+    tidur : function (jam){
+        this.energi += jam * 2;
+        console.log(`Halo ${this.nama}, selamat tidur`)
     }
+};
+
+    function Mahasiswa(nama, energi){
+        let mahasiswa = {};
+        mahasiswa.nama = nama;
+        mahasiswa.energi = energi;
+        mahasiswa.tidur = tidur;
+
     return mahasiswa;
 }
 let tazakka = Mahasiswa('Tazakka', 20)
 console.log(tazakka)
+
