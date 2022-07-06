c6433fd774d37393e793f7be47093548786cc6e5
tazakka <mymac@Jacks-MacBook-Pro.local>
2022-07-06 18:56:22 +0700

    challange6 new

--- challange/challange6.js
+++ challange/challange6.js
@@ -1,16 +1,14 @@
 function sentencesManipulation(sentence) {
     const vokal = ["a", "i", "u", "e", "o"];
     let words = sentence.split(" ");
-    let newWords = words.map((word) => {
-      if (vokal.includes(word.charAt(0))) {
-        return word;
+    let newWords = [];
+    for (let index = 0; index < words.length; index++){
+      if (vokal.includes(words[index].charAt(0))){ /*tadi kurang index*/
+        newWords.push(words[index])
       } else {
-        word = word.substring(1) + word.charAt(0) +'nyo'
-        return word;
+        words[index] = words[index].substring(1) + words[index].charAt(0) +'nyo'
+        newWords.push(words[index])
       }
-    });
-    console.log(newWords.join(" "))
+    } console.log(newWords.join(' '))
   }
   sentencesManipulation('ibu pergi ke pasar bersama aku')
-  
-  
