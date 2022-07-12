adfadee71ae69d0a0163feb2d2c9029599fd30e1
tazakka <mymac@Jacks-MacBook-Pro.local>
2022-07-12 22:37:08 +0700

    coretan guys

--- coretan/coretan3.js
+++ coretan/coretan3.js
@@ -1,14 +1,40 @@
-
-var squaredNum = 1;                 //define squareNum variable
-
-function square(num) {
-  
-  for(i = num; i > 0; i--){         // loop the same number of times as the number entered
-    squaredNum = squaredNum * num   // everytime we loop add number to itself
-     
-  } 
-  console.log(squaredNum);          // spit out squared number 
+const matrix = (n) => {
+    const results = [];
+    for (let i = 0; i < n; i++) {
+        results.push([i]);
+    }
+    let counter = 1;
+    let startColumn = 0;
+    let endColumn = n - 1;
+    let startRow = 0;
+    let endRow = n - 1;
+    while (startColumn <= endColumn && startRow <= endRow) {
+        // Top row
+        for (let i = startColumn; i <= endColumn; i++) {
+            results[startRow][i] = counter;
+            counter++;
+        }
+        startRow++;
+        // Right column
+        for (let i = startRow; i <= endRow; i++) {
+            results[i][endColumn] = counter;
+            counter++;
+        }
+        endColumn--;
+        // Bottom row
+        for (let i = endColumn; i >= startColumn; i--) {
+            results[endRow][i] = counter;
+            counter++;
+        }
+        endRow--;
+        // start column
+        for (let i = endRow; i >= startRow; i--) {
+            results[i][startColumn] = counter;
+            counter++;
+        }
+        startColumn++;
+    }
+    return results;
 }
 
-square(5);                          // call function and give argument
-
+console.log(matrix(5))
