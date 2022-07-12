74fd74a8d04656d98a7b23b845e5af341289a6f1
tazakka <mymac@Jacks-MacBook-Pro.local>
2022-07-12 22:46:55 +0700

    coretan

--- coretan/coretan.js
+++ coretan/coretan.js
@@ -1,14 +1,33 @@
-function weirdMultiply(sentence) {
 
-  let huruf = sentence.toString();
-  let angka = huruf.split('')
-  if (angka.length == 1) {
-    return sentence
-  } else {
-      return weirdMultiply(angka.reduce((previousValue, currentValue) => previousValue * currentValue))
-    }
-  }
-
-console.log(weirdMultiply(39));
-console.log(weirdMultiply(999));
-console.log(weirdMultiply(3))
+        if (arr.length === 0) {
+            return;
+        }
+        if (arr.length === 1) {
+            result = result.concat(arr[0]);
+            return; 
+        }
+        for (let i = 0; i < arr.length; i++) {
+            result.push(arr[0][i]);
+        }
+        arr.shift();
+        for (let i = 0; i < arr.length - 1; i++) {
+            result.push(arr[i][arr[i].length - 1]);
+            arr[i].pop()
+            if (arr[i].length === 0) {
+                arr.splice(i, 1);
+                i--;
+            }
+        }
+        for (let i = arr[arr.length - 1].length - 1; i >= 0; i--) {
+            result.push(arr[arr.length - 1][i]);
+        }
+        arr.pop();
+        for (let i = arr.length - 1; i >= 0; i--) {
+            result.push(arr[i][0]);
+            arr[i].shift();
+            if (arr[i].length === 0) {
+                arr.splice(i, 1);
+            }
+        }
+        readLayer(arr);
+    
