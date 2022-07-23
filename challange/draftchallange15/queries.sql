673950f9fa6bb4cd531a9a5c1ee6745def259af4
Muhammad Tazakka Ramadhan <108384217+mtazakka@users.noreply.github.com>
2022-07-23 19:01:22 +0700

    Delete challange/draftchallange15 directory
    spam

--- challange/draftchallange15/queries.sql
+++
@@ -1,9 +0,0 @@
-select mahasiswa.nim, mahasiswa.nama, mahasiswa.alamat, jurusan.nama from jurusan JOIN mahasiswa ON jurusan.id_jurusan = mahasiswa.id_jurusan;
-
-select DATEDIFF(yy, [tanggallahir], GETDATE()) + (CASE WHEN DATEPART(MONTH, GETDATE()) - DATEPART(MONTH, [tanggallahir])<0 THEN -1 ELSE 0 END) as umur;
-
-tanggallahir DATE NOT NULL,
-    DATEDIFF(yy, [tanggallahir], GETDATE()) + (CASE WHEN DATEPART(MONTH, GETDATE()) - DATEPART(MONTH, [tanggallahir])<0 THEN -1 ELSE 0 END) as umur,
-
-     select nama, tanggallahir, strftime('%Y', date('now')) - strftime ('%Y', date('tanggallahir')) as 'age' from mahasiswa;
-     select nama, tanggallahir, date('now') from mahasiswa;
