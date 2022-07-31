87655cea9a456379e5f920af3e4d2b6fa47b57af
Muhammad Tazakka Ramadhan <108384217+mtazakka@users.noreply.github.com>
2022-07-31 09:30:21 +0700

    update

--- coretan/newcoretan.js
+++ coretan/newcoretan.js
@@ -1,8 +1,7 @@
 var Table = require('cli-table');
 var table = new Table({ chars: {'mid': '', 'left-mid': '', 'mid-mid': '', 'right-mid': ''} });
 table.push(
-    ['foo', 'bar', 'baz']
-  , ['frobnicate', 'bar', 'quuz']
+    
 );
 
 console.log(table.toString());
