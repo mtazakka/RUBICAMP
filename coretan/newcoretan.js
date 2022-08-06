2c2da0af9f978c34b0a0f38385e321019b3d135f
tazakka <mymac@Jacks-MacBook-Pro.local>
2022-08-06 20:03:22 +0700

    update

--- coretan/newcoretan.js
+++
@@ -1,7 +0,0 @@
-var Table = require('cli-table');
-var table = new Table({ chars: {'mid': '', 'left-mid': '', 'mid-mid': '', 'right-mid': ''} });
-table.push(
-    
-);
-
-console.log(table.toString());
