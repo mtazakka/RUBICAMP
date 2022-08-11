f9c335511d23639856f15a56d37c1875f7eb7d77
tazakka <mymac@Jacks-MacBook-Pro.local>
2022-08-11 22:24:52 +0700

    11 Aug

--- challange/webdev/challange21/app.js
+++ challange/webdev/challange21/app.js
@@ -19,12 +19,12 @@ var indexRouter = require('./routes/index')(pool);
 var usersRouter = require('./routes/users');
 
 var app = express();
-var bodyParser = require('body-parser')
-// parse application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({ extended: false }))
+// var bodyParser = require('body-parser')
+// // parse application/x-www-form-urlencoded
+// app.use(bodyParser.urlencoded({ extended: false }))
 
-// parse application/json
-app.use(bodyParser.json())
+// // parse application/json
+// app.use(bodyParser.json())
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
